2834cd22005a56cb7d5d244199b8c82784b76180
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2017-11-16 20:50:29 +0000

    extra debian packages needed for bioconductor

--- setup/installRDebian.sh
+++ setup/installRDebian.sh
@@ -2,5 +2,5 @@ echo "deb http://cran.rstudio.com/bin/linux/debian stretch-cran34/" >> /etc/apt/
 apt-key adv --keyserver keys.gnupg.net --recv-key 'E19F5F87128899B192B1A2C2AD5F960A256A04AF'
 apt-get update
 apt-get -y --allow-unauthenticated upgrade
-apt-get -y --allow-unauthenticated install r-base libopenblas-base
+apt-get -y --allow-unauthenticated install r-base libopenblas-base libcurl4-openssl-dev libxml2-dev 
 
