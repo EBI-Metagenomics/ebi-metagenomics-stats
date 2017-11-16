f104a5e517744337d3bb3f94b040112486d62ab4
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2017-11-16 19:24:18 +0000

    latest debs

--- setup/installRDebian.sh
+++ setup/installRDebian.sh
@@ -1,4 +1,5 @@
+sudo echo "deb http://cran.rstudio.com/bin/linux/debian stretch-cran34/" >> /etc/apt/sources.list
 sudo apt-get update
 sudo apt-get -y upgrade
-sudo apt-get -y install r-base
+sudo apt-get -y install r-base libopenblas-base
 
