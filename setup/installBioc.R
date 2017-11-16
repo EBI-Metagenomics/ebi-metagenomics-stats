4a4c23c93ab4c2798dd683a0a2b8141747959438
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2017-11-16 20:25:59 +0000

    set userdir

--- setup/installBioc.R
+++ setup/installBioc.R
@@ -1,3 +1,5 @@
+userdir = unlist(strsplit(Sys.getenv('R_LIBS_USER'),.Platform$path.sep))[1L]
 source('http://bioconductor.org/biocLite.R')
-biocLite('phyloseq');biocLite('DESeq2')
+biocLite('phyloseq',lib=userdir)
+biocLite('DESeq2',lib=userdir)
 
