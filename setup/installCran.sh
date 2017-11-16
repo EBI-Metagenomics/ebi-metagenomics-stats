8df24385e4452d44c6feea0405ce328b1a42a9c1
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2017-11-16 18:56:31 +0000

    hard code cran repo and set user lib

--- setup/installCran.sh
+++ setup/installCran.sh
@@ -1,1 +1,2 @@
-Rscript -e "install.packages('ebimetagenomics')"
+Rscript -e "install.packages('ebimetagenomics',repos='https://mirrors.ebi.ac.uk/CRAN/',lib=unlist(strsplit(Sys.getenv('R_LIBS_USER'),.Platform\$path.sep))[1L])"
+
