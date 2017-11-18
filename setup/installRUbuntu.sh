4e9fbff630a1e08d5308376cdaf18722d1e8494a
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2017-11-18 12:58:22 +0000

    tweak of ubuntu script

--- setup/installRUbuntu.sh
+++ setup/installRUbuntu.sh
@@ -1,5 +1,5 @@
-echo "" >> /etc/apt/sources.list
-
+echo "https://cran.rstudio.com/bin/linux/ubuntu artful/" >> /etc/apt/sources.list
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9
 apt-get update
 apt-get -y upgrade
 apt-get -y install r-base libopenblas-base libcurl4-openssl-dev libxml2-dev
