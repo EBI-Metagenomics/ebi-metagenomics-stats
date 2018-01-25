d72748ad0d3e469f0a44c871a82f8e8dc34f2494
Simon Potter <scp@ebi.ac.uk>
2018-01-25 13:48:13 +0000

    added some error handling - ignore runs without OTU file and skip diversity.tsv if empty

--- diversity/diversity.R
+++ diversity/diversity.R
@@ -19,21 +19,33 @@ tab = NULL
 for (dir in dirlist) {
     dirname = strsplit(dir,.Platform$file.sep)[[1]][-1]
     run = strsplit(dirname,'_')[[1]][1]
-    message(run)
     otufile = file.path(dirname,"cr_otus",
                         paste(dirname,"otu_table.txt",sep="_"))
-    otu = read.otu.tsv(otufile)
-    svg(paste(run,"svg",sep="."))
-    summ = analyseOtu(otu)
-    dev.off()
-    tab = rbind(tab,summ)
-    rownames(tab)[dim(tab)[1]] = run
-    file.rename(from=paste(run,"svg",sep="."),
-                to=file.path(dirname,"charts","tad-plots.svg"))
+    if (! file.exists(otufile)) {
+       # message(paste("Skipping", run))
+        cat("Skipping ",run,"\n",sep="")
+        next
+    }
+    message(run)
+    tryCatch({
+        otu = read.otu.tsv(otufile)
+        svg(paste(run,"svg",sep="."))
+        summ = analyseOtu(otu)
+        dev.off()
+        tab = rbind(tab,summ)
+        rownames(tab)[dim(tab)[1]] = run
+        file.rename(from=paste(run,"svg",sep="."),
+                    to=file.path(dirname,"charts","tad-plots.svg"))
+       cat("Processed ",run,"\n",sep="")
+    }, error = function(cond) {if (file.exists(paste(run,"svg",sep="."))) file.remove(paste(run,"svg",sep=".")); cat("Failed on run ",run,": ",cond,"\n",sep="")})
 }
 df=data.frame("Run"=rownames(tab),tab)
-write.table(df,file.path("project-summary","diversity.tsv"),
+if (nrow(df)) {
+    write.table(df,file.path("project-summary","diversity.tsv"),
             sep="\t",row.names=FALSE)
+}else
+    message("No diversity data available")
+cat("Completed ",currentdir,"\n",sep="")
 
 
 ## eof
